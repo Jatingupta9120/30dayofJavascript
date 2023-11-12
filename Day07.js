45cd592fe69ad6d4bc3e398b9071c9a07515c736
Jatin Gupta <Jatinofficial9120@gmail.com>
2023-11-12 19:44:52 +0530

    day07 completed

---
+++ Day07.js
@@ -0,0 +1,8 @@
+function test50(x, y) 
+{
+  return ((x == 50 || y == 50) || (x + y == 50));
+}
+console.log(test50(50, 50))
+console.log(test50(20, 50))
+console.log(test50(20, 20))
+console.log(test50(20, 30))
